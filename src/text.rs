027c4ecce9a6b677895b1307c3520753266f5112
BrightShard <brightshard@brightshard.dev>
2026-02-02 20:36:10 -0500

    update to rust 2024 edition

--- src/text.rs
+++ src/text.rs
@@ -229,8 +229,8 @@ impl CowSpan<'_> {
 	#[inline]
 	pub fn as_str(&self) -> &str {
 		match self {
-			Self::Raw(ref raw) => &raw.source[raw.start..=raw.end],
-			Self::Modified(_, ref modified) => modified,
+			Self::Raw(raw) => &raw.source[raw.start..=raw.end],
+			Self::Modified(_, modified) => modified,
 		}
 	}
 
@@ -238,8 +238,8 @@ impl CowSpan<'_> {
 	#[inline]
 	pub fn span(&self) -> &Span<'_> {
 		match self {
-			Self::Raw(ref span) => span,
-			Self::Modified(ref span, _) => span,
+			Self::Raw(span) => span,
+			Self::Modified(span, _) => span,
 		}
 	}
 }
